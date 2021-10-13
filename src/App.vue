d3f87bb24041c1fbb0749187b99fca94417ad279
XantX <hp_xantx@hotmail.com>
2021-10-13 17:02:43 -0500

    Se integro el bootstrap al proyecto de vue TODO: implementar las secciones como componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="card shadow-lg my-4 mx-5" style="width: 18rem;">
+      <img src="./assets/logo.png" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">Tarjeta de prueba</h5>
+        <p class="card-text">El bootstrap funciona</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
