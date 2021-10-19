6057de490a2b2410013d413ca5b12abcd13a601b
XantX <hp_xantx@hotmail.com>
2021-10-19 13:04:32 -0500

    update deploy link

--- deploy-vue.bat
+++ deploy-vue.bat
@@ -4,5 +4,5 @@ cd dist
 call git add -A
 set version=%1
 call git commit -m "version %version%"
-call git push -f git@github.com:XantX/work-portfolio.git master:gh-pages
+call git push -f https://github.com/XantX/work-portfolio.git master:gh-pages
 cd ..
