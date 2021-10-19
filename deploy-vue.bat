8c9f3ed5aee1c650120c848428d769c53143fffe
XantX <hp_xantx@hotmail.com>
2021-10-19 13:34:52 -0500

    version

--- deploy-vue.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call npm run build
-call git add dist -f
-set version=%1
-call git commit -m "version %version%"
-call git subtree push --prefix dist origin gh-pages 
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath:'/work-portfolio/'
-}
